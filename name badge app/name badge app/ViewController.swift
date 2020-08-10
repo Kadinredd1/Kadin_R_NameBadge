7790ddd18ac2703644e86edc18cc612b67d50c5f
Kadinredd1 <67976031+Kadinredd1@users.noreply.github.com>
2020-08-10 13:40:13 -0500

    Add files via upload

---
+++ name badge app/name badge app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  name badge app
+//
+//  Created by Kadin Redd on 8/2/20.
+//  Copyright © 2020 Kadin Redd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
